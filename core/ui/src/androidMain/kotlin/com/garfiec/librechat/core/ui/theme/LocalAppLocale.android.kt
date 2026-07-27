39223e8a69985ed32f92dd528a8694e388b3525a
Wolfram Siener <wolframs91@gmail.com>
2026-07-27 02:21:49 +0200

    fix(i18n): observe Android locale changes

--- core/ui/src/androidMain/kotlin/com/garfiec/librechat/core/ui/theme/LocalAppLocale.android.kt
+++ core/ui/src/androidMain/kotlin/com/garfiec/librechat/core/ui/theme/LocalAppLocale.android.kt
@@ -3,7 +3,7 @@ package com.garfiec.librechat.core.ui.theme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ProvidedValue
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalResources
 import java.util.Locale
 
 actual object LocalAppLocale {
@@ -11,13 +11,13 @@ actual object LocalAppLocale {
 
     actual val current: String
         @Composable
-        get() = Locale.getDefault().toString()
+        get() = LocalConfiguration.current.locales[0].toString()
 
     @Composable
     actual infix fun provides(value: String?): ProvidedValue<*> {
         val configuration = LocalConfiguration.current
         if (default == null) {
-            default = Locale.getDefault()
+            default = configuration.locales[0]
         }
         val new = when (value) {
             null -> default!!
@@ -25,7 +25,7 @@ actual object LocalAppLocale {
         }
         Locale.setDefault(new)
         configuration.setLocale(new)
-        val resources = LocalContext.current.resources
+        val resources = LocalResources.current
         @Suppress("DEPRECATION")
         resources.updateConfiguration(configuration, resources.displayMetrics)
         return LocalConfiguration provides configuration
